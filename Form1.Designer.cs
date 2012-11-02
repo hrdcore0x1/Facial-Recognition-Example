8ecc3f260a0d23e519e5c7409e0983bf12c5b5b1
Cory Nance <corysemail@gmail.com>
2012-11-02 08:42:35 -0400

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace FacialRecognition
+{
+    partial class faceRecognition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGrabber = new System.Windows.Forms.Button();
+            this.picWebCam = new System.Windows.Forms.PictureBox();
+            this.chkboxDetectFaces = new System.Windows.Forms.CheckBox();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.picTraining = new System.Windows.Forms.PictureBox();
+            this.lstTraining = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnDetect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTraining)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGrabber
+            // 
+            this.btnGrabber.Location = new System.Drawing.Point(418, 34);
+            this.btnGrabber.Name = "btnGrabber";
+            this.btnGrabber.Size = new System.Drawing.Size(121, 23);
+            this.btnGrabber.TabIndex = 0;
+            this.btnGrabber.Text = "View Webcam";
+            this.btnGrabber.UseVisualStyleBackColor = true;
+            this.btnGrabber.Click += new System.EventHandler(this.btnGrabber_Click);
+            // 
+            // picWebCam
+            // 
+            this.picWebCam.Location = new System.Drawing.Point(48, 25);
+            this.picWebCam.Name = "picWebCam";
+            this.picWebCam.Size = new System.Drawing.Size(300, 300);
+            this.picWebCam.TabIndex = 1;
+            this.picWebCam.TabStop = false;
+            // 
+            // chkboxDetectFaces
+            // 
+            this.chkboxDetectFaces.AutoSize = true;
+            this.chkboxDetectFaces.Location = new System.Drawing.Point(557, 39);
+            this.chkboxDetectFaces.Name = "chkboxDetectFaces";
+            this.chkboxDetectFaces.Size = new System.Drawing.Size(87, 17);
+            this.chkboxDetectFaces.TabIndex = 2;
+            this.chkboxDetectFaces.Text = "Detect faces";
+            this.chkboxDetectFaces.UseVisualStyleBackColor = true;
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Location = new System.Drawing.Point(418, 63);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(121, 23);
+            this.btnTrain.TabIndex = 3;
+            this.btnTrain.Text = "Train";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.btnTrain_Click);
+            // 
+            // picTraining
+            // 
+            this.picTraining.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picTraining.Location = new System.Drawing.Point(418, 92);
+            this.picTraining.Name = "picTraining";
+            this.picTraining.Size = new System.Drawing.Size(100, 108);
+            this.picTraining.TabIndex = 4;
+            this.picTraining.TabStop = false;
+            // 
+            // lstTraining
+            // 
+            this.lstTraining.FormattingEnabled = true;
+            this.lstTraining.Location = new System.Drawing.Point(545, 92);
+            this.lstTraining.Name = "lstTraining";
+            this.lstTraining.Size = new System.Drawing.Size(120, 108);
+            this.lstTraining.TabIndex = 5;
+            this.lstTraining.SelectedIndexChanged += new System.EventHandler(this.lstTraining_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(69, 340);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(150, 340);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(99, 23);
+            this.btnDetect.TabIndex = 7;
+            this.btnDetect.Text = "Detect Faces";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // faceRecognition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 399);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lstTraining);
+            this.Controls.Add(this.picTraining);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.chkboxDetectFaces);
+            this.Controls.Add(this.picWebCam);
+            this.Controls.Add(this.btnGrabber);
+            this.Name = "faceRecognition";
+            this.Text = "Facial Recognition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTraining)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGrabber;
+        private System.Windows.Forms.PictureBox picWebCam;
+        private System.Windows.Forms.CheckBox chkboxDetectFaces;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.PictureBox picTraining;
+        private System.Windows.Forms.ListBox lstTraining;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnDetect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Microsoft.VisualBasic;
+
+
+/*
+ * Author: Cory Nance
+ * Professor: Dr. Cook
+ * Class: CSCI 7130
+ * Artificial Intelligence Semester Project - Facial Recognition
+ * 
+ * REFERENCES:
+ * http://www.codeproject.com/Articles/239849/Multiple-face-detection-and-recognition-in-real-ti
+ * http://docs.opencv.org/trunk/modules/contrib/doc/facerec/
+ * http://code.google.com/p/javacv/source/browse/samples/FaceRecognition.java
+ */
+
+namespace FacialRecognition
+{
+    public partial class faceRecognition : Form
+    {
+        private Capture grab;
+        private HaarCascade face;
+        private List<Image<Gray, byte>> trainingImgs;
+        private List<string> trainingNames;
+        private Image<Bgr, byte> current;
+
+        public faceRecognition()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            face = new HaarCascade("haarcascade_frontalface_default.xml");
+            btnTrain.Enabled = false;
+            trainingImgs = new List<Image<Gray, byte>>();
+            trainingNames = new List<string>();
+            Application.Idle += new EventHandler(FrameGrabber);
+            grab = new Capture();
+            
+        }
+
+        private void btnGrabber_Click(object sender, EventArgs e)
+        {
+            if (btnGrabber.Text == "View Webcam")
+            {
+                grab = new Capture();
+                grab.QueryFrame();
+                btnTrain.Enabled = true;
+                btnGrabber.Text = "Stop Webcam";
+                btnLoad.Enabled = false;
+            }
+            else if (btnGrabber.Text == "Stop Webcam")
+            {
+                btnGrabber.Text = "View Webcam";
+                btnTrain.Enabled = false;
+                grab.Dispose();
+                btnLoad.Enabled = true;
+            }
+
+        }
+
+        private void FrameGrabber(object sender, EventArgs args)
+        {
+            if (btnGrabber.Text == "View Webcam") return; 
+
+            current = grab.QueryFrame().Resize(300, 250, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+            if (chkboxDetectFaces.Checked == true)
+            {
+
+                Image<Gray, byte> grayScale = current.Convert<Gray, byte>();
+
+                MCvAvgComp[][] detected = grayScale.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+                foreach (MCvAvgComp d in detected[0])
+                {
+                    current.Draw(d.rect, new Bgr(Color.LawnGreen), 2);
+                    if (trainingImgs.Count > 0)
+                    {
+                        Image<Gray, byte> dFace = current.Copy(d.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                        MCvTermCriteria criteria = new MCvTermCriteria(trainingImgs.Count, 0.001);  //count, epsilon value
+                        EigenObjectRecognizer recognize = new EigenObjectRecognizer(trainingImgs.ToArray(), trainingNames.ToArray(), ref criteria);
+                        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 1, 1);
+                        string name = recognize.Recognize(dFace);
+                        current.Draw(name, ref font, new Point(d.rect.X - 2, d.rect.Y - 20), new Bgr(Color.Red));
+                    }
+                }
+            }
+            picWebCam.Image = current.ToBitmap();
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> train = grab.QueryGrayFrame().Resize(300, 250, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] detected = train.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            Image<Bgr, byte> current = grab.QueryFrame().Resize(300, 250, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            train = current.Copy(detected[0][0].rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            picTraining.Image = train.ToBitmap();
+            string name = Interaction.InputBox("Please enter a name for this person.", Application.ProductName);
+            if (trainingNames.Contains(name))
+            {
+                MessageBox.Show(name + " already exist in the training database!");
+                picTraining.Image = trainingImgs[lstTraining.SelectedIndex].ToBitmap();
+                return;
+            }
+            if (trainingImgs.Count > 0)
+            {
+                MCvTermCriteria criteria = new MCvTermCriteria(trainingImgs.Count, 0.001);  //count, epsilon value
+                EigenObjectRecognizer recognize = new EigenObjectRecognizer(trainingImgs.ToArray(), trainingNames.ToArray(), ref criteria);
+                string result = recognize.Recognize(train);
+                if (!String.IsNullOrEmpty(result))
+                {
+                    MessageBox.Show(name + "'s picture already exist in the training database as " + result + "!");
+       
+                        picTraining.Image = trainingImgs[lstTraining.SelectedIndex].ToBitmap();
+                        return;
+                    
+                }
+
+            }
+      
+
+            trainingNames.Add(name);
+            lstTraining.Items.Add(name);
+            trainingImgs.Add(train);
+            lstTraining.SelectedIndex = lstTraining.Items.Count - 1;
+        }
+
+        private void lstTraining_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            picTraining.Image = trainingImgs[lstTraining.SelectedIndex].ToBitmap();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result != DialogResult.OK) return;
+            string file = openFileDialog1.FileName;
+            current = new Image<Bgr, byte>(file).Resize(300, 250, INTER.CV_INTER_CUBIC);
+            picWebCam.Image = current.ToBitmap();
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> grayScale = current.Convert<Gray, byte>();
+
+                MCvAvgComp[][] detected = grayScale.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+                foreach (MCvAvgComp d in detected[0])
+                {
+                    current.Draw(d.rect, new Bgr(Color.LawnGreen), 2);
+                    if (trainingImgs.Count > 0)
+                    {
+                        Image<Gray, byte> dFace = current.Copy(d.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                        MCvTermCriteria criteria = new MCvTermCriteria(trainingImgs.Count, 0.001);  //count, epsilon value
+                        EigenObjectRecognizer recognize = new EigenObjectRecognizer(trainingImgs.ToArray(), trainingNames.ToArray(), ref criteria);
+                        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 1, 1);
+                        string name = recognize.Recognize(dFace);
+                        current.Draw(name, ref font, new Point(d.rect.X - 2, d.rect.Y - 20), new Bgr(Color.Red));
+                    }
+                }
+            picWebCam.Image = current.ToBitmap();
+        }
+
+
+
+
+    }
+}
