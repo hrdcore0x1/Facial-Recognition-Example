835ee687fbcf6675829424be56f12e561e801953
Cory Nance <corysemail@gmail.com>
2012-11-02 10:20:54 -0400

    Working.

--- Form1.cs
+++ Form1.cs
@@ -33,6 +33,7 @@ namespace FacialRecognition
         private List<Image<Gray, byte>> trainingImgs;
         private List<string> trainingNames;
         private Image<Bgr, byte> current;
+        private string filePath;
 
         public faceRecognition()
         {
@@ -41,13 +42,15 @@ namespace FacialRecognition
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            face = new HaarCascade("haarcascade_frontalface_default.xml");
+            //face = new HaarCascade("haarcascade_frontalface_default.xml");
+            face = new HaarCascade("C:\\Users\\cory.nance\\Downloads\\haarcascade_frontalface_alt.xml");
             btnTrain.Enabled = false;
             trainingImgs = new List<Image<Gray, byte>>();
             trainingNames = new List<string>();
             Application.Idle += new EventHandler(FrameGrabber);
-            grab = new Capture();
-            
+            current = null;
+            filePath = string.Empty;
+            btnDetect.Enabled = false;
         }
 
         private void btnGrabber_Click(object sender, EventArgs e)
@@ -56,16 +59,19 @@ namespace FacialRecognition
             {
                 grab = new Capture();
                 grab.QueryFrame();
-                btnTrain.Enabled = true;
+                //btnTrain.Enabled = true;
                 btnGrabber.Text = "Stop Webcam";
                 btnLoad.Enabled = false;
+                btnDetect.Enabled = false;
+                btnTrain.Enabled = true;
             }
             else if (btnGrabber.Text == "Stop Webcam")
             {
                 btnGrabber.Text = "View Webcam";
-                btnTrain.Enabled = false;
+                //btnTrain.Enabled = false;
                 grab.Dispose();
                 btnLoad.Enabled = true;
+                btnTrain.Enabled = false;
             }
 
         }
@@ -101,39 +107,59 @@ namespace FacialRecognition
 
         private void btnTrain_Click(object sender, EventArgs e)
         {
-            Image<Gray, byte> train = grab.QueryGrayFrame().Resize(300, 250, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-            MCvAvgComp[][] detected = train.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
-            Image<Bgr, byte> current = grab.QueryFrame().Resize(300, 250, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-            train = current.Copy(detected[0][0].rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-            picTraining.Image = train.ToBitmap();
-            string name = Interaction.InputBox("Please enter a name for this person.", Application.ProductName);
-            if (trainingNames.Contains(name))
+            //current = new Image<Bgr, byte>(new Bitmap(picWebCam.Image));
+            if (btnGrabber.Text == "View Webcam")
             {
-                MessageBox.Show(name + " already exist in the training database!");
-                picTraining.Image = trainingImgs[lstTraining.SelectedIndex].ToBitmap();
+                current = new Image<Bgr, byte>(filePath).Resize(300, 250, INTER.CV_INTER_CUBIC);
+            }
+            else
+            {
+                current = grab.QueryFrame().Resize(300, 250, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            }
+            Image<Gray, byte> train = current.Convert<Gray, byte>();
+            MCvAvgComp[][] detected = train.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            if (detected[0].Length < 1)
+            {
+                MessageBox.Show("There aren't any faces to train!");
                 return;
             }
-            if (trainingImgs.Count > 0)
+            foreach (MCvAvgComp d in detected[0])
             {
-                MCvTermCriteria criteria = new MCvTermCriteria(trainingImgs.Count, 0.001);  //count, epsilon value
-                EigenObjectRecognizer recognize = new EigenObjectRecognizer(trainingImgs.ToArray(), trainingNames.ToArray(), ref criteria);
-                string result = recognize.Recognize(train);
-                if (!String.IsNullOrEmpty(result))
+                train = current.Copy(d.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                picTraining.Image = train.ToBitmap();
+                string name = Interaction.InputBox("Please enter a name for this person.", Application.ProductName);
+
+                /*
+                if (trainingNames.Contains(name))
                 {
-                    MessageBox.Show(name + "'s picture already exist in the training database as " + result + "!");
-       
-                        picTraining.Image = trainingImgs[lstTraining.SelectedIndex].ToBitmap();
-                        return;
-                    
+                    MessageBox.Show(name + " already exist in the training database!");
+                    picTraining.Image = trainingImgs[lstTraining.SelectedIndex].ToBitmap();
+                    return;
                 }
 
-            }
-      
 
-            trainingNames.Add(name);
-            lstTraining.Items.Add(name);
-            trainingImgs.Add(train);
-            lstTraining.SelectedIndex = lstTraining.Items.Count - 1;
+                if (trainingImgs.Count > 0)
+                {
+                    MCvTermCriteria criteria = new MCvTermCriteria(trainingImgs.Count, 0.001);  //count, epsilon value
+                    EigenObjectRecognizer recognize = new EigenObjectRecognizer(trainingImgs.ToArray(), trainingNames.ToArray(),0, ref criteria);
+                    string result = recognize.Recognize(train);
+                    if (!String.IsNullOrEmpty(result))
+                    {
+                        MessageBox.Show(name + "'s picture already exist in the training database as " + result + "!");
+       
+                            picTraining.Image = trainingImgs[lstTraining.SelectedIndex].ToBitmap();
+                            return;
+                    
+                    }
+
+                }
+                */
+
+                trainingNames.Add(name);
+                lstTraining.Items.Add(name);
+                trainingImgs.Add(train);
+                lstTraining.SelectedIndex = lstTraining.Items.Count - 1;
+            }
         }
 
         private void lstTraining_SelectedIndexChanged(object sender, EventArgs e)
@@ -145,13 +171,16 @@ namespace FacialRecognition
         {
             DialogResult result = openFileDialog1.ShowDialog();
             if (result != DialogResult.OK) return;
-            string file = openFileDialog1.FileName;
-            current = new Image<Bgr, byte>(file).Resize(300, 250, INTER.CV_INTER_CUBIC);
+            filePath = openFileDialog1.FileName;
+            btnDetect.Enabled = true;
+            btnTrain.Enabled = true;
+            current = new Image<Bgr, byte>(filePath).Resize(300, 250, INTER.CV_INTER_CUBIC);
             picWebCam.Image = current.ToBitmap();
         }
 
         private void btnDetect_Click(object sender, EventArgs e)
         {
+            current = new Image<Bgr, byte>(filePath).Resize(300, 250, INTER.CV_INTER_CUBIC);
             Image<Gray, byte> grayScale = current.Convert<Gray, byte>();
 
                 MCvAvgComp[][] detected = grayScale.DetectHaarCascade(face, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
@@ -162,7 +191,7 @@ namespace FacialRecognition
                     {
                         Image<Gray, byte> dFace = current.Copy(d.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
                         MCvTermCriteria criteria = new MCvTermCriteria(trainingImgs.Count, 0.001);  //count, epsilon value
-                        EigenObjectRecognizer recognize = new EigenObjectRecognizer(trainingImgs.ToArray(), trainingNames.ToArray(), ref criteria);
+                        EigenObjectRecognizer recognize = new EigenObjectRecognizer(trainingImgs.ToArray(), trainingNames.ToArray(), 0, ref criteria);
                         MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 1, 1);
                         string name = recognize.Recognize(dFace);
                         current.Draw(name, ref font, new Point(d.rect.X - 2, d.rect.Y - 20), new Bgr(Color.Red));
@@ -170,8 +199,6 @@ namespace FacialRecognition
                 }
             picWebCam.Image = current.ToBitmap();
         }
-
-
 
 
     }
